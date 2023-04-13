ed715a2742c162435c0083bf0a4f34e0d0ac5bb4
galkinsden <galkinsden@gmail.com>
2023-04-13 17:37:10 +0300

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# nextjs-example-project
+1. `chmod +x build-and-run.sh`
+2. execute sh script by `bash build-and-run.sh` or `node build-and-run.js`;
+3. Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ build-and-run.js
@@ -0,0 +1,14 @@
+const { exec } = require('child_process');
+
+const executeNextjs = exec('./build-and-run.sh', {}, (error, _, stderr) => {
+  if (error) {
+    console.log(`error: ${error}`);
+  }
+  if (stderr) {
+    console.log(`stderr: ${stderr}`);
+  }
+});
+
+executeNextjs.stdout.on('data', function(data) {
+  console.log(data.toString()); 
+});
